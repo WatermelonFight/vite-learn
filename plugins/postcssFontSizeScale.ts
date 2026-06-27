6311b60353fd391c9fc482ebfe02d8c48915b411
WatermelonFight <1022683550@qq.com>
2026-06-27 23:23:10 +0800

    feat:重构为ts版本

---
+++ plugins/postcssFontSizeScale.ts
@@ -0,0 +1,8 @@
+import type { Rule, Declaration } from "postcss";
+
+export const fontSizeScalePlugin = {
+  postcssPlugin: "postcss-font-size-scale",
+  Declaration(decl: Declaration) {
+    console.log(decl);
+  },
+};
